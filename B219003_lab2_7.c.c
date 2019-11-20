f60b1736ad8068e7bc3519921c0b7132a06c0a04
AishwaryaMishra123 <57992900+AishwaryaMishra123@users.noreply.github.com>
2019-11-20 19:29:42 +0530

    Add files via upload

---
+++ B219003_lab2_7.c.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    printf("\nA A A A A   I   S S S S S");
+    printf("\nA       A   I   S");
+    printf("\nA       A   I   S");
+    printf("\nA A A A A   I   S S S S S");
+    printf("\nA       A   I           S");
+    printf("\nA       A   I           S");
+    printf("\nA       A   I   S S S S S");
+    return 0;
+}
